55b9f23d4e9748c877d21693f87cd83a59e63a83
Elom Gomez <gomezelom@yahoo.com>
2016-01-30 17:57:15 -0600

    wip

--- lib/elder_wand/rails/helpers/helpers.rb
+++ lib/elder_wand/rails/helpers/helpers.rb
@@ -1,52 +1,84 @@
 module ElderWand
   module Rails
     module Helpers
+      attr_reader :access_token # [OAuth2::AccessToken]
       extend ActiveSupport::Concern
-      rescue_from Oauth2::Error, with: :render_strategy_error
+      rescue_from Oauth2::Error, with: :elder_wand_render_strategy_error
 
       def authenticate_user!
         user = User.find_for_database_authentication(username: params[:username])
         if user && user.valid_password?(params[:password])
-          create_access_token!
+          @access_token = elder_wand_create_access_token!(params[:code], user.id, ElderTree.configuration.scopes)
         else
-          render_invalid_password_error
+          elder_wand_render_strategy_error_with()
         end
       end
 
-      def authorize_client! *scopes
+      def authorize_client!(*scopes)
+        @elder_tree_scopes = scopes.presence || ElderWand.configuration.default_scopes
+        valid_elder_tree_client?
       end
 
-      def authorize_user! *scopes
-      end
-
-      def create_access_token!
-        @access_token = client.auth_code.get_token(params[:code])
-        # client.token_from_auth_code(params[:code])
+      def authorize_user!(*scopes)
+        @elder_tree_scopes = scopes.presence || ElderWand.configuration.default_scopes
+        valid_elder_tree_token?
       end
 
       def current_resource_owner
-        raise "some error if access_toke is nil"
-        @current_resource_owner ||= User.find(access_token.resource_owner_id) if access_token
+        # raise "access_token not present in request header[:authorization] or params" unless access_token
+        # raise "not authorized user" unless access_token
+        raise ElderWand::Errors::InvalidAccessToken unless access_token
+        @current_resource_owner ||= User.find(access_token.resource_owner_id)
       end
 
-      # def client
-      #   @client ||= ElderWand::OAuth2.new
-      # end
+      def elder_wand_create_access_token!(code, resource_owner_id, scopes)
+        options = {
+          resource_owner_id: resource_owner_id
+          scopes: scopes
+        }
+        client.token_from_auth_code(code, options)
+      end
 
-      # @return [OAuth2::AccessToken] the initalized AccessToken
-      # def access_token
-      #   @access_token
-      # end
+      private
 
-      def client
-        @client ||= ElderWand::Client.new(
+      def elder_wand_client
+        @elder_wand_client ||= ElderWand::Client.new(
           params[:client_id],
           params[:client_secret],
           site: 'http://api.hogwarts.dev'
         )
       end
 
-      def render_invalid_password_error
+      def valid_elder_tree_token?
+        @access_token = elder_wand_get_token_info
+        @access_token && @access_token.acceptable?(@elder_tree_scopes)
+      end
+
+      def valid_elder_tree_client?
+        @client_app = elder_wand_get_client_app_info
+        # @client_app && @client_app.acceptable?(@elder_tree_scopes)
+      end
+
+      def elder_wand_get_token_info
+        token = elder_wand_token_from_params || elder_wand_token_from_bearer_auth
+        elder_wand_client.get_token_info(token)
+      end
+
+      def elder_wand_token_from_params
+        params[:token]
+      end
+
+      def elder_wand_token_from_bearer_auth
+        pattern = /^Bearer /i
+        header  = request.authorization
+        header.gsub pattern, '' if match_header_pattern?(header, pattern)
+      end
+
+      def match_header_pattern?(header, pattern)
+        header && header.match(pattern)
+      end
+
+      def elder_wand_render_invalid_password_error
         status = 401
         render content_type: 'application/json',
                status: status,
@@ -60,7 +92,7 @@ module ElderWand
       end
 
       # @param [OAuth2::Error] exception the error response body
-      def render_strategy_error(exception)
+      def elder_wand_render_strategy_error(exception)
         response = exception.response
         status = response.status
 
