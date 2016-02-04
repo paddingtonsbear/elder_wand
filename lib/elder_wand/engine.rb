af74ac6cfec416735aeb99da6412eec4fbe15f6e
Elom Gomez <gomezelom@yahoo.com>
2016-02-03 21:27:39 -0600

    add more intializer methods to elder wand engine

--- lib/elder_wand/engine.rb
+++ lib/elder_wand/engine.rb
@@ -2,20 +2,20 @@ module ElderWand
   class Engine < ::Rails::Engine
     isolate_namespace ElderWand
 
-    # initializer "elder_wand.params.filter" do |app|
-    #   app.config.filter_parameters += [:client_secret, :code, :token]
-    # end
-    #
-    # initializer "elder_wand.locales" do |app|
-    #   if app.config.i18n.fallbacks.blank?
-    #     app.config.i18n.fallbacks = [:en]
-    #   end
-    # end
-    #
-    # initializer "elder_wand.helpers" do
-    #   ActiveSupport.on_load(:action_controller) do
-    #     include ElderWand::Rails::Helpers
-    #   end
-    # end
+    initializer "elder_wand.params.filter" do |app|
+      app.config.filter_parameters += [:client_secret, :code, :token]
+    end
+
+    initializer "elder_wand.locales" do |app|
+      if app.config.i18n.fallbacks.blank?
+        app.config.i18n.fallbacks = [:en]
+      end
+    end
+
+    initializer "elder_wand.helpers" do
+      ActiveSupport.on_load(:action_controller) do
+        include ElderWand::Rails::Helpers
+      end
+    end
   end
 end
