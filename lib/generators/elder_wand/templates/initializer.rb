37b9b35d9c9dd9313b1f1355d8bceab000a3caa1
Elom Gomez <gomezelom@yahoo.com>
2016-09-28 19:53:14 -0500

    Add redirect_uri to configurator and update test helpers

--- lib/generators/elder_wand/templates/initializer.rb
+++ lib/generators/elder_wand/templates/initializer.rb
@@ -18,4 +18,11 @@ ElderWand.configure do
     #   raise ElderWand::Errors::InvalidPasswordError
     # end
   end
+
+  # Change the native redirect uri for client apps
+  # When clients register with the following redirect uri, they won't be redirected to any server and the authorization code will be displayed within the provider
+  # The value can be any string. Use nil to disable this feature. When disabled, clients must provide a valid URL
+  # (Similar behaviour: https://developers.google.com/accounts/docs/OAuth2InstalledApp#choosingredirecturi)
+  #
+  redirect_uri 'urn:ietf:wg:oauth:2.0:oob'
 end
